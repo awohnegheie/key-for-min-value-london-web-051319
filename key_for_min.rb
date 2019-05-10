10620aa800d4d6256ac105cdb5779319262c5dba
Awoh Negheie <awohnegheie@gmail.com>
2019-05-10 01:34:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,15 +7,15 @@ name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 
 def key_for_min_value(name_hash)
-  lowest_key = nil 
-  lowest_value = nil 
+  lowest_key = nil
+  lowest_value = nil
   name_hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
-      lowest_value = v 
-      lowest_key = k 
-    end 
-  end 
+      lowest_value = v
+      lowest_key = k
+    end
+  end
   lowest_key
-end 
+end
 
 key_for_min_value(name_hash)
